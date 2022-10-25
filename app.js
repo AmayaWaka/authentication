a602e45ff59900b17cb3d706c571dc738c6252cf
AmayaWaka <rmadegwa63@gmail.com>
2022-10-25 10:04:24 +0100

    Reset

--- app.js
+++ app.js
@@ -4,44 +4,26 @@ const bodyParser = require("body-parser")
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session  = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
 
 
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-//Using sessions
-app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
-}));
-//passport
-app.use(passport.initialize());
-//Tells passport to use session
-app.use(passport.session());
 
 
 //Created db using connection string
 mongoose.connect("mongodb://localhost:27017/userDB");
-//Initialized db constants
-const userSchema = new mongoose.Schema({
+const userSchema = {
   email: String,
   password: String
-});
 
-//Adding passportLocalMongoose plugin
-userSchema.plugin(passportLocalMongoose);
+};
+
+
 
 const User = mongoose.model("user", userSchema);
 
-passport.use(User.createStrategy());
-//serializing and deserializing user
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
