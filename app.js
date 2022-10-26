18bcd1f2f113213a3b7b3ecb16f75dc5e38348e8
AmayaWaka <rmadegwa63@gmail.com>
2022-10-26 08:59:15 +0100

    Added git ignore

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 
 
@@ -14,11 +17,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //Created db using connection string
 mongoose.connect("mongodb://localhost:27017/userDB");
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
+});
 
-};
+
+userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields: ["password"] });
+
 
 
 
@@ -35,13 +41,44 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
   res.render("register");
 });
-app.post("/register", function(req, res){
+app.get("/secrets", function(req, res){
+  res.render("secrets");
 
+});
+app.post("/register", function(req, res){
+  User.findOne({email: req.body.username}, function(foundItems){
+    if(foundItems){
+      res.redirect("login");
+    }else{
+      const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+      });
+      newUser.save();
+    }
+
+  });
 });
 //Geting data from the post routes
 
 app.post("/login", function(req, res){
+User.findOne({email: req.body.username}, function(err, foundUser){
+  if(err){
+    res.send(err);
+  }else{
+    if(foundUser){
+      if(foundUser.password === req.body.password){
+        res.redirect("secrets");
+      }else{
+        res.send("Wrong password");
+      }
 
+    }else{
+      res.send("User not found");
+    }
+  }
+
+});
 
 });
 
